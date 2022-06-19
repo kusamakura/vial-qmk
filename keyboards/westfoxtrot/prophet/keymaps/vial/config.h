73c0b5e452cdcef50f34d3cc37854745e35cd540
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-06-19 18:38:57 +0900

    Add westfoxtrot/prophet

---
+++ keyboards/westfoxtrot/prophet/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x43, 0xD3, 0xE6, 0x56, 0xC7, 0xAE, 0x1F, 0x0D}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
+
+#define VIAL_COMBO_ENTRIES 16
+#define VIAL_TAP_DANCE_ENTRIES 16
