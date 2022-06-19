7b677ad1a9411a30956b24a1507a45932c7ef875
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-06-19 18:35:57 +0900

    Add boardwalk

---
+++ keyboards/boardwalk/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0x49, 0xAA, 0x19, 0xC9, 0x35, 0x4B, 0x02}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
+
+#define VIAL_COMBO_ENTRIES 16
