6466feb35887ad3e3e2686905f40356a8faf14a8
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-06-19 18:34:58 +0900

    Add dailycraft/claw44 (right side main)

---
+++ keyboards/dailycraft/claw44/keymaps/vial_right/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define MASTER_RIGHT
+
+#define VIAL_KEYBOARD_UID {0x12, 0x94, 0x49, 0x09, 0xB5, 0x5B, 0x28, 0x62}
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 5, 6 }
+
+#define VIAL_COMBO_ENTRIES 16
+#define VIAL_TAP_DANCE_ENTRIES 16
