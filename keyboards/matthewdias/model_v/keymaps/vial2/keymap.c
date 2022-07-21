019b2b2bca1aa2432ca86bd3e45123686890c978
kusamakura <882152+kusamakura@users.noreply.github.com>
2022-07-22 02:29:53 +0900

    matthewdias/model_v: Add sticky mods

--- keyboards/matthewdias/model_v/keymaps/vial2/keymap.c
+++ keyboards/matthewdias/model_v/keymaps/vial2/keymap.c
@@ -17,6 +17,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+  LCK_SHIFT = USER00,
+  LCK_CTRL,
+  LCK_ALT,
+  LCK_CMD,
+  LCK_OFF
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split(
         KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,    KC_P,    KC_BSPC,
@@ -46,3 +54,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LCK_OFF:
+      if (record->event.pressed) {
+        clear_mods();
+      }
+      break;
+    case LCK_SHIFT:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LSFT)) {
+          unregister_mods(MOD_BIT(KC_LSFT));
+        }
+        else {
+          register_mods(MOD_BIT(KC_LSFT));
+        }
+      }
+      break;
+    case LCK_CTRL:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LCTL)) {
+          unregister_mods(MOD_BIT(KC_LCTL));
+        }
+        else {
+          register_mods(MOD_BIT(KC_LCTL));
+        }
+      }
+      break;
+    case LCK_ALT:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LALT)) {
+          unregister_mods(MOD_BIT(KC_LALT));
+        }
+        else {
+          register_mods(MOD_BIT(KC_LALT));
+        }
+      }
+      break;
+    case LCK_CMD:
+      if (record->event.pressed) {
+        if (get_mods() & MOD_BIT(KC_LGUI)) {
+          unregister_mods(MOD_BIT(KC_LGUI));
+        }
+        else {
+          register_mods(MOD_BIT(KC_LGUI));
+        }
+      }
+      break;
+  }
+  return true;
+}
